955cbc9c310e46c6c351683dc7e376f3c1f567cf
abansybana09 <abansybana322@gmail.com>
2025-10-13 19:06:56 +0700

    -

--- app/page.tsx
+++ app/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
           <div className="flex items-center gap-5">
             <motion.div whileHover={{ scale: 1.05 }}>
               <Avatar className="size-24 shadow-lg ring-4 ring-amber-200">
-                <AvatarImage src="/avatar.jpg" alt="Your name" />
+                <AvatarImage src="/lufy.jpg" alt="Your name" />
                 <AvatarFallback>SI</AvatarFallback>
               </Avatar>
             </motion.div>
